e0fa8fb4dcc940558703a0bb667026a3016b359f
TushitaTahsin <kazi26@student.sust.edu>
2022-01-22 15:03:31 +0600

    Added Certificate page

--- training_frontend/src/views/Course/Certificate.vue
+++ training_frontend/src/views/Course/Certificate.vue
@@ -1,7 +1,7 @@
 <template>
-  <h1 class="pt-10 text-2xl font-semibold">Add Course</h1>
-  <div class="flex items-center justify-center mb-20">
-    <form @submit.prevent="addCourse">
+  <h1 class="pt-10 text-2xl font-semibold">Generate certificate</h1>
+  <div class="flex items-center justify-center mb-10">
+    <form @submit.prevent="">
       <div class="space-y-5">
         <div class="grid grid-rows-1 gap-2 place-items-start">
           <label for="custName" class="font-semibold ml-2"
@@ -79,21 +79,60 @@
       </div>
     </form>
   </div>
-  <div class="flex items-center justify-center relative">
-    <img src="../../assets/images/certificatebg.png" alt="logo" />
-    <h1 class="absolute text-white text-9xl font-greatVibes mb-16">
-      {{ custName }}
-    </h1>
-    <h1 class="absolute text-white text-4xl mt-24">
-      has been awarded this certificate for successfully
-    </h1>
-    <h1 class="absolute text-white text-4xl mt-44">
-      completing the course of {{ courseName }}
-    </h1>
+
+  <div class="flex justify-center">
+    <div ref="receiptContent">
+      <div class="flex items-center justify-center relative w-1120 h-830">
+        <img
+          src="../../assets/images/certificatebg1.png"
+          alt="logo"
+          class="w-1120 h-830"
+        />
+        <h1 class="absolute text-white text-8xl font-greatVibes mb-48">
+          {{ custName }}
+        </h1>
+        <h1 class="absolute text-white text-3xl mt-16">
+          has been awarded this certificate for successfully
+        </h1>
+        <h1 class="absolute text-white text-3xl mt-36">
+          completing the course of {{ courseName }}
+        </h1>
+        <h1 class="absolute text-white text-2xl left-20 bottom-24">
+          {{ getCertDate }}
+        </h1>
+        <img
+          class="absolute w-96 h-96 -bottom-5"
+          src="../../assets/images/3.png"
+          alt="signature"
+        />
+        <h1 class="absolute text-certificateColor1 text-sm bottom-48 font-bold">
+          {{ courseNameShort }}
+        </h1>
+        <h1
+          class="absolute text-certificateColor1 text-2xl bottom-36 font-bold"
+        >
+          {{ getYear }}
+        </h1>
+        <img
+          class="absolute right-20 bottom-24 w-24 h-24"
+          src="../../assets/images/shajalvai.png"
+          alt="signature"
+        />
+      </div>
+    </div>
   </div>
+  <button
+    class="px-4 py-2 rounded-md bg-green bg-green-200 hover:bg-green-300 my-10"
+    @click="downloadReceipt"
+  >
+    Download receipt
+  </button>
 </template>
 
 <script>
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+
 export default {
   mounted() {
     this.$store.dispatch("getCourseList");
@@ -103,9 +142,50 @@ export default {
     return {
       custName: "",
       date: "",
+      certDate: "",
       courseName: "",
       courseNameShort: "",
+      month: "",
+      day: "",
+      year: "",
     };
+  },
+
+  methods: {
+    downloadReceipt() {
+      const doc = new jsPDF({
+        orientation: "l",
+        unit: "px",
+        format: "a4",
+        hotfixes: ["px_scaling"],
+      });
+
+      html2canvas(this.$refs.receiptContent, {
+        width: doc.internal.pageSize.getWidth(),
+        height: doc.internal.pageSize.getHeight(),
+      }).then((canvas) => {
+        const img = canvas.toDataURL("image/png");
+
+        doc.addImage(
+          img,
+          "PNG",
+          0,
+          0,
+          doc.internal.pageSize.getWidth(),
+          doc.internal.pageSize.getHeight()
+        );
+        let filename =
+          this.custName +
+          "_" +
+          this.courseNameShort +
+          "_" +
+          new Date().toLocaleDateString() +
+          "_" +
+          new Date().toLocaleTimeString() +
+          ".pdf";
+        doc.save(filename);
+      });
+    },
   },
 
   computed: {
@@ -113,6 +193,42 @@ export default {
       get() {
         return this.$store.getters.courseList;
       },
+    },
+
+    getCertDate() {
+      if (this.date) {
+        let d = new Date(this.date);
+        let year = d.getFullYear();
+        let month = d.toLocaleString("en-us", { month: "long" });
+        let day = d.getDate();
+        this.certDate =
+          month.toUpperCase() + " " + day.toString() + " - " + year.toString();
+      } else {
+        this.certDate = "";
+      }
+      return this.certDate;
+    },
+
+    courseNameShort() {
+      if (this.courseName == "System Design Level 1") {
+        this.courseNameShort = "Sys Design 1";
+      } else if (
+        this.courseName == "Cracking the Coding Interview with Leetcode"
+      ) {
+        this.courseNameShort = "Leetcode";
+      } else {
+        this.courseNameShort = this.courseName;
+      }
+      return this.courseNameShort;
+    },
+    getYear() {
+      if (this.date) {
+        let d = new Date(this.date);
+        this.year = d.getFullYear();
+      } else {
+        this.year = "";
+      }
+      return this.year;
     },
   },
 };
--- training_frontend/tailwind.config.js
+++ training_frontend/tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         receiptTextColor: "#004D80",
         receiptBorder: "#1B9DC9",
         receiptInputBorder: "#707070",
+        certificateColor1: "#242b32",
       },
       width: {
         500: "500px",
@@ -16,16 +17,22 @@ module.exports = {
         400: "400px",
         800: "800px",
         1100: "1130px",
+        1120: "1125px",
+      },
+      height: {
+        830: "800px",
       },
       fontFamily: {
         segoeUI: "'Segoe UI'",
         poppins: "'poppins', sans-serif",
         greatVibes: "'Great Vibes', cursive",
+        MyriadProBold: "'MyriadPro Bold'",
       },
       spacing: {
         68: "270px",
         // 35: "-80px",
         100: "420px",
+        46: "180px",
       },
     },
   },
