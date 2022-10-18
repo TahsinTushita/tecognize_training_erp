7019707ac47cd37017ae1cab68549fe9aa55d637
TahsinTushita <tahsintushita@gmail.com>
2022-10-18 13:29:42 +0600

    Changed static file dir in settings.py

--- tecognize_training_erp/urls.py
+++ tecognize_training_erp/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 # from django.urls import path
 # from django.urls import include, re_path
 from django.urls import re_path as url,include
+from django.conf import settings
+from django.conf.urls.static import static
 # from django.conf.urls import url, include
 
 urlpatterns = [
@@ -24,3 +26,5 @@ urlpatterns = [
     url("admin/", admin.site.urls),
     url(r"^", include("training_backend.urls")),
 ]
+
+urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
