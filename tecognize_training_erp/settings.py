a93da5be8190946c35cc3dc9fd50cee9c72ccd34
TahsinTushita <tahsintushita@gmail.com>
2022-10-18 15:52:08 +0600

    added cors origin list

--- tecognize_training_erp/settings.py
+++ tecognize_training_erp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-n@iobuun&+)k3^haq&q=&)r2(aaq^ibie(@gy(pic4af^%fa7@
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ["103.121.60.222","erp.tecognize.com"]
+ALLOWED_HOSTS = ["103.121.60.222","erp.tecognize.com","berp.tecognize.com"]
 
 
 # Application definition
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = "tecognize_training_erp.urls"
 
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ("http://localhost:8081",)
+CORS_ORIGIN_WHITELIST = ("http://localhost:8081")
 
 TEMPLATES = [
     {
@@ -144,3 +144,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8081',
+    'http://127.0.0.1:8000',
+    'http://103.121.60.222',
+    'http://erp.tecognize.com',
+    'http://berp.tecognize.com',
+    'http://wwww.erp.tecognize.com',
+    'http://wwww.berp.tecognize.com',
+    'https://erp.tecognize.com',
+    'https://berp.tecognize.com',
+    'https://www.erp.tecognize.com',
+    'https://www.berp.tecognize.com',
+    'https://www.facebook.com',
+    'https://www.linkedin.com',
+]
